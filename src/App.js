e5732f5dfd47bb013e046bb6c1eb69b80503f13b
Molkamimi <benothmen.molka@gmail.com>
2020-11-02 11:29:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState,useEffect} from 'react'
-import{moviesData} from "./Constants/data";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { moviesData } from "./Constants/data";
+import "./App.css";
 import MovieList from "./Components/MovieList";
 import AddMovie from "./Components/Add/Add";
 import Filter from "./Components/Filter/Filter";
 
-import MovieCard from './Components/MovieCard/MovieCard';
+// import MovieCard from "./Components/MovieCard/MovieCard";
+// import Description from "./Components/Description";
+// import AppRouter from "./router/router";
 
 function App() {
- 
   const [movies, setMovies] = useState(moviesData);
   const [textSerach, setTextSerach] = useState("");
   const [searchRate, setSearchRate] = useState(0);
   const handleAdd = (newMovie) => {
-    
     if (newMovie.name !== "") {
       setMovies([...movies, newMovie]);
     }
@@ -21,30 +21,21 @@ function App() {
   return (
     <div>
       <div className="navmenu">
-       
-        
         <Filter
-        rating={searchRate}
-        setSearchRate={setSearchRate}
-       setTextSerach={setTextSerach}
+          rating={searchRate}
+          setSearchRate={setSearchRate}
+          setTextSerach={setTextSerach}
         />
-        
-       
-        
-        
-        <AddMovie handleAdd={handleAdd}/>
-        
-      
-        </div>
-         
+
+        <AddMovie handleAdd={handleAdd} />
+      </div>
+
       <MovieList
-       searchRate={searchRate}
+        searchRate={searchRate}
         textSerach={textSerach}
         movies={movies}
-       />
-     
-      
-</div>
+      />
+    </div>
   );
 }
 
